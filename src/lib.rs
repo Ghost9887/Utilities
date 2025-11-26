019a5ec842b450c28a2d784b802a24f0c23fd04f
Ghost <duchojakub987@gmail.com>
2025-11-26 14:57:28 +0100

    improvementes

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
-use std::process;
-
 pub enum Commands {
     Cat,
 }
 
-pub fn get_command(args: &Vec<String>) -> String {
-    println!("{:?}", args);
+pub fn get_command(command_arg: &str) -> Result<Commands, String> {
+    println!("{:?}", command_arg);
 
-    let mut command = String::new();
+    match command_arg {
+        "cat" => Ok(Commands::Cat),
+        _ => Err(format!("Uknown command: '{}'", command_arg))
+    }
 
-    command = String::from("cat");
-    
-    command
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use std::{env, fs};
-use utilities::{get_command};
+use std::{env, fs, process};
+use utilities::{get_command, Commands};
 
 fn main() {
     
     //store the given args
     let args: Vec<String> = env::args().collect();
 
+    if args.len() < 2 {
+        eprintln!("No arguments passed.");
+        process::exit(1);
+    }
+
     //find what command to run
-    let command = get_command(&args);
+    let command = match get_command(&args[1]) {
+        Ok(command) => command,
+        Err(error) => {
+            eprintln!("{error}");
+            process::exit(1);
+        } 
+    };
 
-    println!("{command}");
+    //execute the command
+    process_command(command, args);
+}
 
+fn process_command(command: Commands, args: Vec<String>) {
+    println!("processing the command");
 }
